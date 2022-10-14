2e96ef5f58fa0fcad581a99ade5192d3c1d9fe3f
Klin <piter_frst@yahoo.com>
2022-10-14 21:37:08 +0300

    Task 009 N shows even from 1+N

---
+++ Task_009_N_shows_even_from_1+N/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Clear();
+
+Console.WriteLine("Введите ваше число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{num} -> ");
+
+for (int i = 2; i <= num; i = i + 2)
+{
+    Console.Write($" {i} ");
+}
