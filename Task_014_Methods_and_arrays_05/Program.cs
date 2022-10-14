2bc4798f7da56c74f86fed1b0d415d04851a682d
Klin <piter_frst@yahoo.com>
2022-10-14 21:39:32 +0300

    Task 014 Methods and arrays 05

---
+++ Task_014_Methods_and_arrays_05/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно число квадратом другого.
+
+*/
+Console.Clear();
+
+Console.WriteLine("Введите ваше первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите ваше второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void SquareOfAnother(int num1, int num2)
+{
+    if (num1 * num1 == num2)
+    { 
+        Console.WriteLine($"{num1} является квадратом {num2}");
+        return;
+    }
+    if (num2 * num2 == num1)
+    {
+        Console.WriteLine($"{num2} является квадратом {num1}");
+        return; 
+    }
+    Console.WriteLine("Ни чё не вышло... =((");
+}   
+
+SquareOfAnother(num1, num2);
