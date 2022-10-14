c45a60ebb1e52240d7b1a24eb86253d13288443b
Klin <piter_frst@yahoo.com>
2022-10-14 21:42:29 +0300

    Task 019 Sum of nums from 1 to A

---
+++ Task_019_Sum_of_nums_from_1toA/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число (A)
+и выдаёт сумму чисел от 1 до A.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//метод суммы чисел от числа.
+int GetSumOfInterval(int number)
+{
+    int result = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int sum = GetSumOfInterval(num);
+Console.WriteLine($"{num} -> {sum}");
