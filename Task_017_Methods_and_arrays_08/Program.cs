4b729b6a94398df6ce4ffe04daf7f195fbd7a97e
Klin <piter_frst@yahoo.com>
2022-10-14 21:40:57 +0300

    Task 017 Methods and arrays 08

---
+++ Task_017_Methods_and_arrays_08/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+
+AB = √(xb - xa)2 + (yb - ya)2
+*/
+Console.Clear();
+
+Point GetPoint()
+{
+    Point point = new Point();
+    Console.WriteLine("Введите координату X:");
+    point.X = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y:");
+    point.Y = int.Parse(Console.ReadLine());
+
+    return point;
+}
+
+Point firstPoint = GetPoint();
+Point secondPoint = GetPoint();
+
+double result = Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y, 2));
+Console.WriteLine($"{result}\n");
+
+class Point
+{
+    public int X { get; set;}
+    public int Y { get; set;}
+}
