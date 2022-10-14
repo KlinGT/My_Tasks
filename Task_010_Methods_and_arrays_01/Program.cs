762451f60fc47bb8f6bfcea3a31b69ab47f78521
Klin <piter_frst@yahoo.com>
2022-10-14 21:37:37 +0300

    Task 010 Methods and arrays 01

---
+++ Task_010_Methods_and_arrays_01/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+Console.Clear();
+
+int GiveMyNumber(int min, int max)        //метод получения рандомного числа.
+{
+    Random rnd = new Random();
+    int result = rnd.Next(min, max);
+    return result;
+}
+
+int GetMaxDigit(int number)                 //метод получения максимальной цифры из числа.
+{
+    int num1 = number / 10;                 //95 / 10 = 9
+    int num2 = number % 10;                 //95 % 10 = 5
+
+    if(num1 > num2)
+        return num1;
+    else 
+        return num2;
+}
+
+int number = GiveMyNumber(10, 100);
+int MaxDigit = GetMaxDigit(number);  
+
+Console.WriteLine($"{number} -> {MaxDigit}"); 
