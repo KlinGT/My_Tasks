881f2686d9de8b027088b7d4f6f8d9869134bf76
Klin <piter_frst@yahoo.com>
2022-10-14 21:38:35 +0300

    Task 012 Methods and arrays 03

---
+++ Task_012_Methods_and_arrays_03/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Напишите программу, которая будет на вход принимать два числа
+и выводить является ли второе число кратным первому. 
+Если число 2ое некратно число 1му, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите ваше первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите ваше второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void MultipleOfFirst(int num1, int num2)
+{
+    int num3 = num1 % num2;
+    if (num3 == 0)
+        Console.WriteLine($"{num1}, {num2} -> кратно");
+    else
+        Console.WriteLine($"{num1}, {num2} -> не кратно, остаок {num3}");
+}   
+
+MultipleOfFirst(num1, num2);
+
+/*Вариант из семинара:
+Console.Clear();
+
+void CompareNum(int num1, int num2)
+{
+    int num3 = num1 % num2;
+    if (num3 == 0)
+        Console.WriteLine("Второе число кратное.");
+    else
+        Console.WriteLine($"Второе число не кратное первому. Остаток {num3}");
+}
+CompareNum(15,4);
+*/
