9a218580446c9393f67486fb835dbcba7f61e5e5
Klin <piter_frst@yahoo.com>
2022-10-14 21:45:29 +0300

    Task 021 Product of nums from 1 to N

---
+++ Task_021_Product_of_nums_from_1toN/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 28: 
+Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N. 
+
+*/
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//метод произведения чисел от числа.
+int GetNumOf(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int res = GetNumOf(num);
+Console.WriteLine($"{num} -> {res}");
