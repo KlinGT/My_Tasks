2c8c71db2576d90936cb850abae38b50a6b18fd2
Klin <piter_frst@yahoo.com>
2022-10-14 21:33:13 +0300

    Task 002 First square of second

---
+++ Task_002_First_square_of_second/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на входе принимает два числа и проверяет,
+является ли первое число квадратом второго.
+а = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+
+*/
+
+Console.WriteLine("Введите первое число: ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_B = Convert.ToInt32(Console.ReadLine());
+
+if (num_B * num_B == num_A)
+    Console.WriteLine($"a = {num_A}, b = {num_B} -> да");
+else
+    Console.WriteLine($"a = {num_A}, b = {num_B} -> нет");
