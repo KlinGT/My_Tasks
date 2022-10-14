c914716e9ae4659180900f3b60e4f787da9811cf
Klin <piter_frst@yahoo.com>
2022-10-14 21:39:07 +0300

    Task 013 Methods and arrays 04

---
+++ Task_013_Methods_and_arrays_04/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+Console.Clear();
+
+Console.WriteLine("Введите ваше число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void MultipleOfTwo(int num)
+{
+    int first = num % 7;
+    int second = num % 23;
+    if (first == 0 & second == 0)
+        Console.WriteLine($"{num} -> да");
+    else
+        Console.WriteLine($"{num} -> нет");
+}   
+
+MultipleOfTwo(num);
