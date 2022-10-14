e047830c04cf1c6dec8dbdb4a4dfc1e5818584b4
Klin <piter_frst@yahoo.com>
2022-10-14 21:32:36 +0300

    Task 001 Square of the number

---
+++ Task_001_Square_of_the_number/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая на входе принимает число и выдаёт его квадрат 
+(число умноженное на само себя)
+*/
+
+Console.Clear();   
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number * number;
+
+//Console.WriteLine("Квадрат вашего числа: ");
+//Console.WriteLine(result);
+
+//вариант вывода одной строкой через ${}:
+Console.WriteLine($"Квадрат вашего числа: {result}");
+
+/*
+
+//Вариант преподователя:
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+
+//получаем числовое значение
+int number = Convert.ToInt32(numberStr);
+int result = number * number;
+
+//варианты вывода ответа:
+//Console.WriteLine(result);
+//Console.WriteLine(number + " -> " + result);
+Console.WriteLine($"{number} -> {result}");
+*/
