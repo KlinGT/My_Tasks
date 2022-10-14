dbe5270dba70b481b72b2a15236cd2931cd04442
Klin <piter_frst@yahoo.com>
2022-10-14 21:41:41 +0300

    Task 018 Methods and arrays 09

---
+++ Task_018_Methods_and_arrays_09/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1, 4.
+*/
+Console.Clear();
+
+void GetNumber (int num)
+{
+    Console.Write($"{num} -> ");
+    for (int i = 1; i <= num; i++)
+    {
+        var squere = Math.Pow(i, 2);
+        Console.Write($"{squere}, ");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+GetNumber(number);
