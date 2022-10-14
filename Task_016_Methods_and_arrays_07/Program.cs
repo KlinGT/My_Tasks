7818ce16f833b387aface440cecfe5ce60ad6314
Klin <piter_frst@yahoo.com>
2022-10-14 21:40:26 +0300

    Task 016 Methods and arrays 07

---
+++ Task_016_Methods_and_arrays_07/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+Console.Clear();
+
+//получаем четверть не равную 0 и не больше 4рёх
+int GetQuarter(string message) 
+{
+    int result = 0;                                     //создаём переменную которую будем возвращать.
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);                     //просим вывести сообщение которое пришло к нам на функцию (стр. 38)
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0 && result <= 4)
+            isCorrect = true;
+        else
+            Console.WriteLine("Введите нужную четверть!\n"); 
+    }
+    return result;
+}
+
+//сообщаем диапозон координат, исходя из номера четверти
+void GetRange(int number)
+{
+    if (number == 1)
+        Console.WriteLine("X от 0 до +бесконечности, Y от 0 до +бесконечности\n");
+    if (number == 2)
+        Console.WriteLine("X от 0 до -бесконечности, Y от 0 до +бесконечности\n");
+    if (number == 3)
+        Console.WriteLine("X от 0 до -бесконечности, Y от 0 до -бесконечности\n");
+    if (number == 4)
+        Console.WriteLine("X от 0 до +бесконечности, Y от 0 до -бесконечности\n");
+}
+
+int num = GetQuarter("Введите номер четверти:");
+GetRange(num);
