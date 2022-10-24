b1a7fa3871cca9be93b61c779669fdd3e51d4009
Klin <piter_frst@yahoo.com>
2022-10-24 15:13:59 +0300

    Array built-in functionality

---
+++ Array_built-in_functionality/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("\n");
+}
+
+int[] array = new int[10];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 10);
+}
+
+int[] resultArray = new int[10];
+Console.WriteLine("Массив resultArray до копирования:");
+PrintArray(resultArray);
+
+Array.Copy(array, resultArray, 10);
+
+Console.WriteLine("Массив resultArray после копирования:");
+PrintArray(resultArray);
+
+Array.Reverse(resultArray);
+
+Console.WriteLine("Массив resultArray после реверса:");
+PrintArray(resultArray);
